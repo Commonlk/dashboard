cd51cb3ff75640b0d0eee935ecb0d63163d1625d
Victor Azevedo <vitinsilva0xd@gmail.com>
2021-01-26 11:33:04 -0300

    Minor changes to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import Head from "next/head";
+import React, { useState } from "react";
 import Header from "../components/header";
 import Card from "../components/card";
 import OrderPopup from "../components/order_popup";
 import { compareValues, excelConverter } from "../js/excelData";
 
 export default function Home({ data }) {
+  const [isActive, setActive] = useState(false);
+  const [details, setDetails] = useState({});
+
   return (
     <>
       <Head>
@@ -13,7 +17,6 @@ export default function Home({ data }) {
       </Head>
 
       <div className="page-container max-w-screen ">
-        {/* <OrderPopup list={lista} /> */}
         <div className="content-wrap">
           <Header />
           <main className="flex w-full max-h-screen">
@@ -29,17 +32,26 @@ export default function Home({ data }) {
                   R$10,00.
                 </p>
               </div>
-              <button className="btn bg-green-300 text-white">Enviar arquivo</button>
+              <button className="btn bg-green-300 text-white">
+                Enviar arquivo
+              </button>
               <p className="py-1 text-lg font-semibold">Ou</p>
               <button className="btn">Selecione arquivo</button>
             </div>
             <div className="flex-grow p-4">
-              <h3 className="text-3xl font-medium text-gray-800">
-                Resultado
-              </h3>
+              <h3 className="text-3xl font-medium text-gray-800">Resultado</h3>
               <div className="grid grid-cols-2 gap-6 my-8 overflow-y-scroll h-4/5">
                 {compareValues(data).map((item, index) => {
-                  return <Card key={item.id} id={item.id} />;
+                  return (
+                    <Card
+                      key={item.id}
+                      id={item.id}
+                      lot={item.lot}
+                      priceA={item.prices.priceA}
+                      priceB={item.prices.priceB}
+                      diff={item.difference}
+                    />
+                  );
                 })}
               </div>
             </div>
@@ -53,6 +65,7 @@ export default function Home({ data }) {
 
 export async function getStaticProps() {
   const data = excelConverter();
+
   return {
     props: {
       data,
