28c9d70f4d31d690186124833f4ebcaa84c27b80
Victor Azevedo <vitinsilva0xd@gmail.com>
2021-01-26 11:29:53 -0300

    Add dynamic return, change from flex display to a table

--- components/order_popup.js
+++ components/order_popup.js
@@ -1,19 +1,16 @@
-function PopupItem(item) {
+function PopupItem(props) {
   return (
-    <div className="flex">
-      <p className="text-lg mr-4">
-        <span className="text-lg">Produto:</span>
-        {item.product}
-      </p>
-      <p className="text-lg mr-4">
-        <span className="text-lg">Quantidade:</span>
-        {item.qty}
-      </p>
-      <p className="text-lg">
-        <span className="text-lg">Valor Liquido:</span>
-        {item.liqValue}
-      </p>
-    </div>
+    <>
+      <tr>
+        <td className="text-lg">{props.product}</td>
+        <td className="text-lg">{props.qty}</td>
+        <td className="text-lg">R$ {parseFloat(props.liqValueA).toFixed(2)}</td>
+        <td className="text-lg">R$ {parseFloat(props.liqValueB).toFixed(2)}</td>
+        <td className="text-lg">
+          R$ {Math.abs(props.liqValueA - props.liqValueB).toFixed(2)}
+        </td>
+      </tr>
+    </>
   );
 }
 
@@ -21,11 +18,30 @@ export default function OrderPopup(props) {
   return (
     <>
       <div className="fixed w-screen h-screen bg-black opacity-50 z-10"></div>
-      <div className="popup-list flex flex-col">
+      <div className="popup-list grid grid-cols-1">
         <button className="absolute top-1 right-3 text-2xl">X</button>
-        {props.list.forEach((item) => {
-          <PopupItem props={item} />;
-        })}
+        <h4 className="text-lg font-medium mb-3">
+          Informações do Pedido: {props.id}
+        </h4>
+        <table className="table-auto">
+          <thead className="popup-table">
+            <tr>
+              <th>Produto</th>
+              <th>Quantidade</th>
+              <th>Valor Liq. Unitário A</th>
+              <th>Valor Liq. Unitário B</th>
+              <th>Diferença</th>
+            </tr>
+          </thead>
+          <tbody className="text-center">
+            <PopupItem
+              product={props.product}
+              qty={props.qty}
+              liqValueA={props.liqValueA}
+              liqValueB={props.liqValueB}
+            />
+          </tbody>
+        </table>
       </div>
     </>
   );
